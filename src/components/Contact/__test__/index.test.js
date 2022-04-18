8021c59bbc2ad698b933756d716ec9d1b70770d9
Twigster <treshagaye@gmail.com>
2022-04-18 00:02:13 -0400

    created modal component

--- src/components/Contact/__test__/index.test.js
+++ src/components/Contact/__test__/index.test.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Contact from '../'
-import ContactForm from '../'
+import ContactForm from '../Contact'
 
 afterEach(cleanup)
 
@@ -29,6 +28,3 @@ describe('Contact component', () => {
 
 })
 
-describe('contact me', () => {
-    
-})
